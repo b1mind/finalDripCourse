632764988c93174c75d9999e9c4d28619b201ea4
b1mind <brent@1mindsolutions.com>
2020-05-24 01:41:30 -0600

    add animation test

--- final.js
+++ final.js
@@ -2,19 +2,43 @@ const header = document.getElementsByTagName('header')[0]
 const nav = document.getElementsByTagName('nav')[0]
 const canHazBurger = document.querySelector('.canHazBurger')
 const hero = document.querySelector('.hero')
+const services = document.querySelectorAll('.services .cell')
+const aside = document.querySelectorAll('.breakDown .cell')
 
 const headerObsOptions = {
 	root: null,
 	threshold: 0,
 	rootMargin: '-420px 0px 0px 0px',
 }
+
 const headerObserver = new IntersectionObserver(function (entries, observer) {
 	entries.forEach(e => {
 		header.classList.toggle('scrolled', !e.isIntersecting)
 	})
 }, headerObsOptions)
 
+const cellsObsOptions = {
+	root: null,
+	threshold: 0,
+	rootMargin: '0px 0px 0px 0px',
+}
+
+const cellsObserver = new IntersectionObserver(function (entries, observer) {
+	entries.forEach(e => {
+		let cell = e.target
+		gsap.fromTo(cell, 1, { autoAlpha: 0, x: '-100%' }, { autoAlpha: 1, x: 0 })
+	})
+}, cellsObsOptions)
+
 headerObserver.observe(hero)
+
+aside.forEach(e => {
+	cellsObserver.observe(e)
+})
+
+services.forEach(e => {
+	cellsObserver.observe(e)
+})
 
 canHazBurger.addEventListener('click', e => {
 	nav.classList.toggle('hidden')
